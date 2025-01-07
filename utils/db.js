d981084367f282cf330d28a675939fdc399fa5bf
mkbhru <mkbhru@gmail.com>
2025-01-07 10:26:31 +0530

    final deployment ready

--- utils/db.js
+++ utils/db.js
@@ -1,24 +1,53 @@
 import mysql from "mysql2/promise";
 
+
 // Create the MySQL connection pool
 const db = mysql.createPool({
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // host: "localhost",
-  // port: 3306,
-  // user: "root",
-  // password: "rootpassword",
-  // database: "jobs_cms",
-
-  host: "mysql-12597ac8-manishmpib-e39d.g.aivencloud.com",
-  port: 28682,
-  user: "avnadmin",
-  password: "AVNS_8AqbwFtQj3tjh3GPcrH",
-  database: "jobs_cms",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+// Add a debugging connection check
+async function checkConnection() {
+  try {
+    const [rows] = await db.query("SELECT 1");
+    console.log("Database connection successful:", rows);
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }
+}
+
+// Run the connection check
+checkConnection();
+
+export default db;
+
+
+
+// import mysql from "mysql2/promise";
+
+// // Create the MySQL connection pool
+// const db = mysql.createPool({
+//   // host: process.env.DB_HOST,
+//   // port: process.env.DB_PORT,
+//   // user: process.env.DB_USER,
+//   // password: process.env.DB_PASSWORD,
+//   // database: process.env.DB_NAME,
+//   // host: "localhost",
+//   // port: 3306,
+//   // user: "root",
+//   // password: "rootpassword",
+//   // database: "jobs_cms",
+
+//   host: "mysql-12597ac8-manishmpib-e39d.g.aivencloud.com",
+//   port: 28682,
+//   user: "avnadmin",
+//   password: "AVNS_8AqbwFtQj3tjh3GPcrH",
+//   database: "jobs_cms",
+// });
 // console.log(process.env.DB_HOST);
 
 // Add a debugging connection check
@@ -34,4 +63,4 @@ const db = mysql.createPool({
 // // Run the connection check
 // checkConnection();
 
-export default db;
+// export default db;
